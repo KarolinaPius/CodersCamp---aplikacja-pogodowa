8450bde7e49ff293f9cbef1a203a0f80d4a63b73
Krzysiek Karczewski <krzysztofkarczewski92@gmail.com>
2019-11-21 06:19:14 +0100

    store API Key as a constant

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-
 const searchButton = document.getElementById('submit');
 const newCity = document.getElementById('city');
 const resultWeather = document.getElementsByClassName('currentWeather');
+const APIKey = "34b55e81e4919626be452ad5a44c606a"
 
-searchButton.onclick = function(){
+searchButton.onclick = function () {
     const city = newCity.value;
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=34b55e81e4919626be452ad5a44c606a`;
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${APIKey}`;
     fetch(url)
-    .then(response => {
-        response.json()
-    .then(json => {
-        let data = json;
-        let output = formatResponse(data);
-        resultWeather.innerHTML = output;
+        .then(response => {
+            response.json()
+                .then(json => {
+                    let data = json;
+                    let output = formatResponse(data);
+                    resultWeather.innerHTML = output;
+                });
         });
-    });
 }
 
-function formatResponse (data) {
+function formatResponse(data) {
     let out = `<p>Aktualna pogoda dla ${city.name}:</p><br/>
     <p>temperatura: ${list[1].main.temp}</p><br/>
     <p>wiatr: ${list[4]}</p><br/>
