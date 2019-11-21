0fc0b07e6eb717c69d5a9bca5a976367d48560ce
Krzysiek Karczewski <krzysztofkarczewski92@gmail.com>
2019-11-21 10:37:24 +0100

    fix api url and current weather fetch

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const APIKey = "34b55e81e4919626be452ad5a44c606a"
 
 const fillWithSearchResult = () => {
     const city = newCity.value;
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${APIKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`;
     fetch(url)
         .then(response => {
             response.json()
                 .then(json => {
                     let data = json;
                     let output = formatResponse(data);
+                    console.log(data)
                     resultWeather.appendChild(output);
                 });
         });
@@ -31,13 +32,13 @@ const getCurrentTime = () => {
 
 function formatResponse(data) {
     let weatherDetails = document.createElement('div')
-    let out = `<p>${getCurrentDate()} ${getCurrentTime()}
-    <p>Aktualna pogoda dla ${data.city.name}:</p><br/>
-    <p>temperatura: ${data.list[0].main.temp}</p><br/>
-    <p>wiatr: ${data.list[0].wind.speed}</p><br/>
-    <p>ciśnienie: ${data.list[0].main.pressure}</p><br/>
-    <p>wilgotność: ${data.list[0].main.humidity}</p><br/>
-    <p>zachmurzenie: ${data.list[0].clouds.all}%</p><br/>`;
+    let out = `<p>${getCurrentDate()} ${getCurrentTime()}</p>
+    <p>Aktualna pogoda dla ${data.name}:</p><br/>
+    <p>temperatura: ${data.main.temp}</p><br/>
+    <p>wiatr: ${data.wind.speed}</p><br/>
+    <p>ciśnienie: ${data.main.pressure}</p><br/>
+    <p>wilgotność: ${data.main.humidity}</p><br/>
+    <p>zachmurzenie: ${data.clouds.all}%</p><br/>`;
     weatherDetails.innerHTML = out
     return weatherDetails;
 }
